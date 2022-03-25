6edfba4579c2a7e01d849430ecf800fdc7957ccc
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-03-24 20:34:24 -0500

    fixing typo

--- seeds/product-tag-seeds.js
+++ seeds/product-tag-seeds.js
@@ -42,22 +42,22 @@ const productTagData = [
     tag_id: 5,
   },
   {
-    id: 8,
+    id: 9,
     product_id: 4,
     tag_id: 1,
   },
   {
-    id: 9,
+    id: 10,
     product_id: 4,
     tag_id: 2,
   },
   {
-    id: 10,
+    id: 11,
     product_id: 4,
     tag_id: 8,
   },
   {
-    id: 11,
+    id: 12,
     product_id: 5,
     tag_id: 3,
   },
